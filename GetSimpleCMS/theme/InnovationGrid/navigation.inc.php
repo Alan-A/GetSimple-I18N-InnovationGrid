94020fb4d37f8dbaad7c25b6536f2b6c70e341e8
Alan-A <Alan-A@gmx.de>
2022-03-10 11:35:44 +0100

    #13 - Indicate current page in menus

--- GetSimpleCMS/theme/InnovationGrid/navigation.inc.php
+++ GetSimpleCMS/theme/InnovationGrid/navigation.inc.php
@@ -14,9 +14,11 @@
 	<div class="section">
 		
 		<?php //get_i18n_component('sidebarnav'); ?>					<!-- 'Standard' version -->
-		<?php get_i18n_component('feule_menuaside', $language, '', 3); ?>	<!-- 'FEULE' version -->
+		<?php $slug = return_page_slug(); ?>
+		<?php get_i18n_component('feule_menuaside', $language, '', 3, $slug); ?>	<!-- 'FEULE' version -->
 											<!-- 3rd parameter is '' or the name of the parent element -->
 											<!-- 4th parameter is the max number of levels to be shown -->
+											<!-- 5th parameter is used to check if page is current/active -->
 		
 	</div>
 	
